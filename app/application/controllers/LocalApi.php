42f53cd59b3021a5e5445f3299e6b4c2fda22817
Mohamed Bayoumi <52928159+Mohamedbaumy@users.noreply.github.com>
2023-06-13 00:19:09 +0100

    updates

--- app/application/controllers/LocalApi.php
+++ app/application/controllers/LocalApi.php
@@ -9,11 +9,17 @@ class LocalApi extends CI_Controller
         $this->load->model('User_model');
         $this->load->database(); // Load the database library
         if ($this->db->conn_id) {
-            echo "Database connection is successful!";
-        } else {
-            echo "Failed to connect to the database.";
+            echo json_encode(
+                array(
+                    'status' => false,
+                    'message' => 'عدد المستخدمين',
+                    'data' => null,
+                    'isAuth' => true
+                ),
+                JSON_UNESCAPED_UNICODE
+            );
+            exit();
         }
-        exit();
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
         header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description, Authorization');
