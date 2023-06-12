7991a4f7c83a0ddf6751626c88e4596a30435326
Mohamed Bayoumi <52928159+Mohamedbaumy@users.noreply.github.com>
2023-06-13 00:14:58 +0100

    updates

--- app/application/libraries/ApiMiddelware.php
+++ app/application/libraries/ApiMiddelware.php
@@ -10,6 +10,7 @@ class ApiMiddelware
         } else {
             echo "Failed to connect to the database.";
         }
+        exit();
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
         header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description, Authorization');
