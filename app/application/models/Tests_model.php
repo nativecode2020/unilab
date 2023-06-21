d30042b467d519393b5f7ce5f370e1c77b01dc4b
Mohamed Bayoumi <52928159+Mohamedbaumy@users.noreply.github.com>
2023-06-21 11:37:51 +0100

    update

--- app/application/models/Tests_model.php
+++ app/application/models/Tests_model.php
@@ -179,7 +179,7 @@ class Tests_model extends CI_Model
         inner join lab_visits on lab_visits.hash = lab_visits_tests.visit_id
         where tests_id='$test' and lab_id='$lab'
         and (name like '%$search%' or visit_date like '%$search%')
-        and lab_visits_tests.isdeleted = 0
+        and lab_visits.isdeleted = 0
         group by lab_visits.hash,tests_id
         order by lab_visits.id desc
         limit $start,$length
@@ -189,7 +189,7 @@ class Tests_model extends CI_Model
         inner join lab_visits on lab_visits.hash = lab_visits_tests.visit_id
         where tests_id='$test' and lab_id='$lab'
         and (name like '%$search%' or visit_date like '%$search%')
-        and lab_visits_tests.isdeleted = 0
+        and lab_visits.isdeleted = 0
         group by lab_visits.hash,tests_id
         order by lab_visits.id desc
         ");
