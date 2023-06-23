0a3c4debf9e79fd2e2ae06a44bd3c7bec29384be
Mohamed Bayoumi <52928159+Mohamedbaumy@users.noreply.github.com>
2023-06-23 19:03:24 +0100

    updates

--- lab/js/exe.js
+++ lab/js/exe.js
@@ -284,82 +284,97 @@ async function getAsyncData() {
     cancelButtonColor: "#d33",
     confirmButtonText: "نعم",
     cancelButtonText: "لا",
-  })
-    .then(async (result) => {
-      // if confirmed then
-      if (result.value) {
-        body.insertAdjacentHTML("beforeend", waitElement);
-        let fromData = new FormData();
-        fromData.append(
-          "date",
-          run(
-            `select insert_record_date as date from system_users_type order by id desc limit 1;`
-          ).result[0]?.query0?.[0]?.date ?? "2023-01-01 00:00:00"
-        );
-        let queries = await fetch(
-          "http://umc.native-code-iq.com/app/index.php/Offline/getAsyncData",
-          {
-            method: "POST",
-            headers: {},
-            body: fromData,
-          }
-        )
-          .then((res) => res.json())
-          .then((res) => {
-            updates = res.updates;
-            inserts = res.inserts;
-            deletes = res.deletes;
-            return res;
-          });
-
-        let updatesTests =
-          run(
-            `select test_name,hash from lab_test where hash in(${updates
-              .map((item) => item.hash)
-              .join(",")});`
-          ).result[0]?.query0 ?? [];
-        const syncBodyModal = document.getElementById("sync_body");
-        if (updatesTests.length > 0) {
-          syncBodyModal.innerHTML += `
-            <div id="update_tests" class="row">
-                <div class="col-12">
-                    <h5 class="text-center"> أختر التحاليل التي تريد تحديثها </h5>
-                </div>
-                ${updatesTests
-                  .map(
-                    (item) =>
-                      `<div class="col-12 border rounded p-2 my-2" style="cursor: pointer;"
-                        data-hash="${item.hash}"
-                        onclick="$(this).toggleClass('active');"
-                       >
-                            <p class="text-center">
-                                <span class="h4">${item.test_name}</span>
-                            </p>
-                        </div>
-                        `
-                  )
-                  .join("")}
-            </div>
-            `;
-        }
-        $("#sync").modal("show");
-        // queries = queries.map((query) => query.query);
-        // let queriesForm = new FormData();
-        // queriesForm.append("queries", JSON.stringify(queries));
-        // let quer = await fetch(`${base_url}LocalApi/run_queries`, {
-        //   method: "POST",
-        //   body: queriesForm,
-        // }).then((res) => res.json());
-        // run(
-        //   `insert into system_users_type (title,insert_record_date) values ('update by ${
-        //     localStorage.getItem("name") ?? ""
-        //   }','${new Date().toISOString().slice(0, 19).replace("T", " ")}');`
-        // );
-      }
+  });
+  body.insertAdjacentHTML("beforeend", waitElement);
+  let fromData = new FormData();
+  fromData.append(
+    "date",
+    run(
+      `select insert_record_date as date from system_users_type order by id desc limit 1;`
+    ).result[0]?.query0?.[0]?.date ?? "2023-01-01 00:00:00"
+  );
+  let queries = await fetch(
+    "http://umc.native-code-iq.com/app/index.php/Offline/getAsyncData",
+    {
+      method: "POST",
+      headers: {},
+      body: fromData,
+    }
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      updates = res.updates;
+      inserts = res.inserts;
+      deletes = res.deletes;
+      return res.inserts;
     })
-    .then(async () => {
+    .then(() => {
       body.removeChild(document.getElementById("alert_screen"));
     });
+
+  let updatesTests =
+    run(
+      `select test_name,hash from lab_test where hash in(${updates
+        .map((item) => item.hash)
+        .join(",")});`
+    ).result[0]?.query0 ?? [];
+  const syncBodyModal = document.getElementById("sync_body");
+  if (updatesTests.length > 0) {
+    syncBodyModal.innerHTML += `
+      <div id="update_tests" class="row">
+          <div class="col-12">
+              <h5 class="text-center"> أختر التحاليل التي تريد تحديثها </h5>
+          </div>
+          ${updatesTests
+            .map(
+              (item) =>
+                `<div class="col-12 border rounded p-2 my-2" style="cursor: pointer;"
+                  data-hash="${item.hash}"
+                  onclick="$(this).toggleClass('active');"
+                 >
+                      <p class="text-center">
+                          <span class="h4">${item.test_name}</span>
+                      </p>
+                  </div>
+                  `
+            )
+            .join("")}
+      </div>
+      `;
+  }
+  $("#sync").modal("show");
+}
+
+async function runAsyncData() {
+  const body = document.getElementsByTagName("body")[0];
+  body.insertAdjacentHTML("beforeend", waitElement);
+  queries = inserts.map((query) => query.query);
+  let updatesSelected = document.querySelectorAll("#update_tests .active");
+  if (updatesSelected.length > 0) {
+    let updateTestsHash = Array.from(updatesSelected).map(
+      (item) => item.dataset.hash
+    );
+    let updateTests = updates.filter((item) =>
+      updateTestsHash.includes(item.hash)
+    );
+    queries = [...queries, ...updateTests.map((item) => item.query)];
+  }
+  console.log(queries);
+  //   let queriesForm = new FormData();
+  //   queriesForm.append("queries", JSON.stringify(queries));
+  //   let quer = await fetch(`${base_url}LocalApi/run_queries`, {
+  //     method: "POST",
+  //     body: queriesForm,
+  //   })
+  //     .then((res) => res.json())
+  //     .then(() => {
+  //       body.removeChild(document.getElementById("alert_screen"));
+  //     });
+  //   run(
+  //     `insert into system_users_type (title,insert_record_date) values ('update by ${
+  //       localStorage.getItem("name") ?? ""
+  //     }','${new Date().toISOString().slice(0, 19).replace("T", " ")}');`
+  //   );
 }
 
 async function updateExpireDate() {
