91d3bda0a97dccf0c98d4a2b7e593f936c9f0377
Mohamed Bayoumi <52928159+Mohamedbaumy@users.noreply.github.com>
2023-07-17 11:55:05 +0300

    update

--- lab/controller/visits.js
+++ lab/controller/visits.js
@@ -2211,7 +2211,7 @@ function addTestToInvoice(
         }
       }
       height += test.eleHeight;
-      if (height <= center2Scroll) {
+      if (height <= center2Scroll +20) {
         invoice.find(".center2 .tester").append(test.html);
         allTestsElements = allTestsElements.slice(1);
       } else {
