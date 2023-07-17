b03ffb7dc106f0445ebcc815c6f599bd7df3faf0
Mohamed Bayoumi <52928159+Mohamedbaumy@users.noreply.github.com>
2023-07-17 11:03:58 +0300

    change in invoice height

--- lab/controller/visits.js
+++ lab/controller/visits.js
@@ -2206,7 +2206,7 @@ function addTestToInvoice(
       if (test?.html?.hasClass("typetest")) {
         lastTestType = test;
         test.html = test.html.clone();
-        if (center2Scroll - height < testTypeHeight + testHeadHeight + 70) {
+        if (center2Scroll - height < testTypeHeight + testHeadHeight + 100) {
           break;
         }
       }
