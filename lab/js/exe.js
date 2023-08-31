80cde3f561a873fcfb9da472b8d837313a9f6589
Mohamed Bayoumi <52928159+Mohamedbaumy@users.noreply.github.com>
2023-08-31 18:13:31 +0300

    ctrl shift r

--- lab/js/exe.js
+++ lab/js/exe.js
@@ -220,6 +220,26 @@ function reloadScripts() {
   }
 }
 
+function hardRefresh() {
+  const t = parseInt(Date.now() / 10000); //10s tics
+  const x = localStorage.getItem("t");
+  localStorage.setItem("t", t);
+
+  if (x != t) location.reload(true); //force page refresh from server
+  else {
+    //refreshed from server within 10s
+    const a = document.querySelectorAll("a, link, script, img");
+    var n = a.length;
+    while (n--) {
+      var tag = a[n];
+      var url = new URL(tag.href || tag.src);
+      url.searchParams.set("r", t.toString());
+      tag.href = url.toString(); //a, link, ...
+      tag.src = tag.href; //rerun script, refresh img
+    }
+  }
+}
+
 async function updateSystem() {
   if (!navigator.onLine) {
     Swal.fire({
@@ -249,7 +269,8 @@ async function updateSystem() {
         confirmButtonText: "موافق",
       }).then((result) => {
         // Call the reloadScripts function to reload all scripts on the page
-        window.location.reload(true);
+        // window.location.reload(true);
+        hardRefresh();
       });
     })
     .catch((error) => {
