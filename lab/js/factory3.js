ca4f301b725a75e55abc3877fb70d8cd36ee91fb
Mohamed Bayoumi <52928159+Mohamedbaumy@users.noreply.github.com>
2024-01-27 19:35:54 +0300

    .

--- lab/js/factory3.js
+++ lab/js/factory3.js
@@ -861,7 +861,10 @@ const waitElement = `<div id="alert_screen" class="alert_screen">
 </div>
 </div>`;
 
-function fireSwal(fun, ...args) {
+function fireSwal(fun = null, ...args) {
+  if (!fun) {
+    console.log("no function");
+  }
   let diffTimeInMin =
     (-1 *
       (new Date(localStorage?.getItem("lastSync") ?? "2023-01-01 00:00:00") -
